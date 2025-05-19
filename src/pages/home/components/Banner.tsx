dd8e18d21df974e11011d22a4c3b19d5998e55b8
chasyuss <cotn963@gmail.com>
2025-05-19 20:37:08 +0900

    fix: [#126] 로그인/비로그인 배너 변경

--- src/pages/home/components/Banner.tsx
+++ src/pages/home/components/Banner.tsx
@@ -1,4 +1,4 @@
-import { HomeBackImage, HomeCard } from './HomeImage';
+import { HomeCard } from './HomeImage';
 import MyDreamArrow from '@assets/icons/myDreamarrow.svg?react';
 import Arrow from '@assets/icons/arrow.svg?react';
 import Bell from '@assets/images/bell.webp';
@@ -10,69 +10,60 @@ const Banner = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-row gap-6">
-      <div className="relative w-full">
-        <div className="z-0">
-          <HomeBackImage />
+    <div className="flex h-[489px] w-full flex-row items-center justify-center space-x-5 bg-purple-150 px-[120px] pb-[50px] pt-[60px]">
+      <div className="relative flex flex-row gap-6">
+        <HomeCard />
+
+        <div className="absolute right-0 flex cursor-pointer items-center rounded-full bg-white py-[6px] pl-4 pr-1">
+          <span className="text-gray-500 font-B02-SB">직업 찾으러 가기</span>
+          <MyDreamArrow />
         </div>
 
-        <div className="absolute left-[122px] top-[100px] z-50">
-          <div className="relative flex flex-row gap-6">
-            <div className="relative">
-              <HomeCard />
+        <div className="absolute left-[30px] top-10 flex flex-col">
+          <span className="text-white font-B02-M">
+            {' '}
+            나에게 딱 맞는 직업은 뭘까?
+          </span>
+          <div className="mt-[10px]">
+            <span className="text-white font-T01-B">
+              인생 2막의 시작은 <br />
+              두드림과 함께 하세요!
+            </span>
+          </div>
+        </div>
+      </div>
 
-              <div className="absolute right-6 top-[14.5px] z-50 flex cursor-pointer items-center space-x-3">
-                <span className="text-gray-500 font-B02-SB">마이드림 가기</span>
-                <MyDreamArrow />
-              </div>
+      <div className="flex h-[379px] w-[440px] flex-col items-start rounded-[30px] border border-gray-300 bg-white p-[30px]">
+        <div className="flex w-full flex-row items-start justify-between">
+          <img src={Bell} alt="Bell" className="h-[60px] w-[60px]" />
+          <div
+            className="flex cursor-pointer flex-row items-center"
+            onClick={() => navigate('/jobsearch')}
+          >
+            <div className="text-gray-500 font-B02-SB"> 채용 정보 보기 </div>
+            <Arrow />
+          </div>
+        </div>
 
-              <div className="absolute left-[29px] top-[40px] flex flex-col">
-                <span className="text-white font-B02-M">58일째 꿈꾸는 중</span>
-                <div className="mt-[10px]">
-                  <span className="text-white font-T01-B">
-                    인생 2막, <br />
-                    어떻게 시작할지 막막하다면?
-                  </span>
-                </div>
-              </div>
-            </div>
+        <span className="mt-5 text-gray-900 font-T01-B">
+          {' '}
+          인기 직종 채용 현황
+        </span>
 
-            <div className="flex h-[465px] w-[384px] flex-col items-start rounded-[30px] border border-gray-300 bg-white px-[30px] py-[40px]">
-              <div className="flex w-full flex-row items-start justify-between">
-                <img src={Bell} alt="Bell" className="h-[76px] w-[76px]" />
+        <div className="mt-5 flex flex-col items-center justify-center">
+          <div className="flex flex-col gap-5">
+            {jobList &&
+              jobList.map((job) => (
                 <div
-                  className="flex cursor-pointer flex-row items-center"
-                  onClick={() => navigate('/jobsearch')}
+                  key={job['job-name']}
+                  className="flex flex-row items-center gap-4"
                 >
-                  <div className="text-gray-500 font-B02-SB">더보기 </div>
-                  <Arrow />
+                  <div className="flex items-center justify-center rounded-[10px] bg-purple-100 p-2 text-purple-500 font-T05-SB">
+                    {job['job-name']}
+                  </div>
+                  <div className="text-gray-900 font-T05-SB">{job.count}건</div>
                 </div>
-              </div>
-
-              <span className="mt-6 text-gray-900 font-T01-B">
-                {' '}
-                인기 직종 구인 현황
-              </span>
-
-              <div className="mt-[27px] flex flex-col items-center justify-center">
-                <div className="flex flex-col gap-4">
-                  {jobList &&
-                    jobList.map((job) => (
-                      <div
-                        key={job['job-name']}
-                        className="flex flex-row items-center gap-4"
-                      >
-                        <div className="flex items-center justify-center rounded-[10px] bg-purple-100 p-2 text-purple-500 font-T05-SB">
-                          {job['job-name']}
-                        </div>
-                        <div className="text-gray-900 font-T05-SB">
-                          {job.count}건
-                        </div>
-                      </div>
-                    ))}
-                </div>
-              </div>
-            </div>
+              ))}
           </div>
         </div>
       </div>
--- src/pages/home/components/HomeImage.tsx
+++ src/pages/home/components/HomeImage.tsx
@@ -1,17 +1,5 @@
-import HomeHeaderImage from '@assets/images/background.png';
-import HomeCardImage from '@assets/images/home_card.png';
-
-export const HomeBackImage = () => {
-  return (
-    <div className="flex w-full items-center justify-center">
-      <img
-        src={HomeHeaderImage}
-        alt="홈헤더 이미지"
-        className="aspect-[96/19] h-[285px] w-full"
-      />
-    </div>
-  );
-};
+import HomeCardImage from '@assets/images/homecard.png';
+import LoginHomeCardImage from '@assets/images/loginhomecard.png';
 
 export const HomeCard = () => {
   return (
@@ -19,7 +7,19 @@ export const HomeCard = () => {
       <img
         src={HomeCardImage}
         alt="홈카드"
-        className="aspect-[402/241] h-[465px] w-[789px]"
+        className="aspect-[789/379] h-[379px] w-[789px]"
+      />
+    </div>
+  );
+};
+
+export const LoginHomeCard = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <img
+        src={LoginHomeCardImage}
+        alt="홈카드"
+        className="aspect-[789/379] h-[379px] w-[789px]"
       />
     </div>
   );
