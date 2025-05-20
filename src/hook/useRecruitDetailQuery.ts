3e9b12ceaf269e6acf15f424279a098eb70beaa1
Choi Yong Won <cywin0821@gmail.com>
2025-05-20 12:11:24 +0900

    feat : [#129] 쿼리에서 필요없는 데이터(디바운스 등)을 제거하고 날짜 관련 정보도 삭제 및 수정

--- src/hook/useRecruitDetailQuery.ts
+++ src/hook/useRecruitDetailQuery.ts
@@ -1,16 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import api from '@hook/api';
 
-export const useRecruitDetailQuery = (id: string) => {
-  return useQuery({
+export const useRecruitDetailQuery = (id: string) =>
+  useQuery({
     queryKey: ['recruitDetail', id],
     queryFn: async () => {
-      const { data } = await api.get('/v1/recruit/detail', {
-        params: { id },
-      });
-      return data.data.job[0]; // job 배열에서 첫 항목만 반환
+      const { data } = await api.get('/v1/recruit/detail', { params: { id } });
+      return data.data.job[0];
     },
     enabled: !!id,
     staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
   });
-};
