461c2fda87870f34a45aa4a7095a92367ca8325c
Choi Yong Won <cywin0821@gmail.com>
2025-05-21 11:27:25 +0900

    feat : [#138] 초기화버튼 스타일 추가되도록 코드 수정

--- src/common/ResetButton.tsx
+++ src/common/ResetButton.tsx
@@ -1,22 +1,26 @@
 import RefreshIcon from '@assets/icons/refresh_icon.svg?react';
+import clsx from 'clsx';
 
 interface ResetButtonProps {
   onClick: () => void;
   label?: string;
+  className?: string;
 }
 
 export default function ResetButton({
   onClick,
   label = '필터 초기화',
+  className,
 }: ResetButtonProps) {
   return (
-    <div className="mb-4 flex justify-end">
-      <button
-        onClick={onClick}
-        className="flex items-center gap-1 rounded-full bg-gray-500 px-4 py-2 text-sm text-white"
-      >
-        {label} <RefreshIcon />
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      className={clsx(
+        'flex items-center gap-1 rounded-full bg-gray-500 px-4 py-2 text-sm text-white',
+        className
+      )}
+    >
+      {label} <RefreshIcon />
+    </button>
   );
 }
