c0b3df40ea72033c8f63cb22280546002ece82da
Choi Yong Won <cywin0821@gmail.com>
2025-05-24 21:17:34 +0900

    feat : [#156] 투두리스트에서 버튼누르면 todoGroupId 넘어가도록 코드 수정

--- src/pages/myTodo/tabs/TodoListPage.tsx
+++ src/pages/myTodo/tabs/TodoListPage.tsx
@@ -2,8 +2,25 @@ import Container from '@pages/myTodo/components/todo/Container.tsx';
 import BackIcon from '@assets/icons/back.svg?react';
 import CautionIcon from '@assets/icons/caution.svg?react';
 import ToggleButton from '@common/Toggle.tsx';
+import { useMdIsPublicMutation } from '@hook/mydream/useMdIsPublicMutation';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const TodoListPage = () => {
+  const { todoGroupId: todoGroupIdParam } = useParams<{ todoGroupId: string }>();
+  const todoGroupId = todoGroupIdParam ? parseInt(todoGroupIdParam, 10) : undefined;
+  const [isPublic, setIsPublic] = useState(false);
+  const { mutate: togglePublicState } = useMdIsPublicMutation();
+
+  const handleToggle = (isPublic: boolean) => {
+    setIsPublic(isPublic);
+    if (todoGroupId) {
+      togglePublicState({ todoGroupId, isPublic });
+    } else {
+      console.error('todoGroupId가 없습니다.');
+    }
+  };
+
   return (
     <div className="mx-[120px] mt-10 bg-gray-50 px-5 py-4">
       <div className="mb-4 flex max-w-[1010px] flex-col gap-2">
@@ -11,14 +28,18 @@ const TodoListPage = () => {
           <BackIcon />
           <input
             placeholder="할일을 입력해주세요"
-            className="w-[974px] rounded-[10px] border border-gray-200 bg-white px-[20px] py-[10px] text-gray-300 font-T05-SB"
+            className="w-[974px] rounded-[10px] border border-gray-200 bg-white px-[20px] py-[10px] text-gray-900 font-T05-SB"
           />
         </div>
 
         <div className="flex content-center items-center gap-1 self-end text-gray-500">
           <CautionIcon className="h-6 w-6" />
           <span className={'text-gray-500 font-B02-M'}>공개</span>
-          <ToggleButton />
+          <ToggleButton 
+            initialState={isPublic}
+            todoGroupId={todoGroupId}
+            onToggle={handleToggle}
+          />
         </div>
       </div>
 
