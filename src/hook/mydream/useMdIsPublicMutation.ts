ff242858d7693d3b1843c492b55d8de7310123e5
Choi Yong Won <cywin0821@gmail.com>
2025-05-24 21:16:10 +0900

    feat : [#156] 토글관련된 훅 (공개 비공개) 하도록 수정

---
+++ src/hook/mydream/useMdIsPublicMutation.ts
@@ -0,0 +1,38 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import api from '@hook/api.ts';
+
+interface TodoPublicResponse {
+  todoGroupId: number;
+  isPublic: boolean;
+  message: string;
+}
+
+export const useMdIsPublicMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async ({
+      todoGroupId,
+      isPublic,
+    }: {
+      todoGroupId: number;
+      isPublic: boolean;
+    }) => {
+      try {
+        const { data } = await api.patch(
+          `/v1/my-dream/todo/${todoGroupId}/public-state`,
+          { isPublic }
+        );
+
+        return data.data as TodoPublicResponse;
+      } catch (error) {
+        throw error;
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['publicState'] });
+    },
+    onError: (error) => {
+      console.error('뮤테이션 에러:', error);
+    },
+  });
+};
