8f81291c64e0b0af4cecaf8e26103402fc0475a8
chasyuss <cotn963@gmail.com>
2025-05-25 00:14:54 +0900

    chore: [#157] 토큰 검증 로직 수정

--- src/hook/useAddJobMutation.ts
+++ src/hook/useAddJobMutation.ts
@@ -3,7 +3,9 @@ import api from './api';
 
 const addJob = async (jobId: number) => {
   const token = localStorage.getItem('accessToken');
-  if (!token) return;
+  if (!token) {
+    throw new Error('인증 토큰이 없습니다. 로그인 후 다시 시도해주세요.');
+  }
 
   const response = await api.post(`/v1/my-dream/job/${jobId}`, null, {
     headers: {
