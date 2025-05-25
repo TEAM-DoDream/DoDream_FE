8e65192b94d2044b118e9883adb51a1ee2be6ced
chasyuss <cotn963@gmail.com>
2025-05-25 13:41:50 +0900

    feat: [#160] 내 정보 연결

---
+++ src/hook/mypage/useMypageQuery.ts
@@ -0,0 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../api';
+
+export interface GetInfoProps {
+  memberId: number;
+  profileImage: string;
+  nickname: string;
+  loginId: string;
+  birthDate: string;
+  regionName: string;
+  jobs: [
+    {
+      jobId: number;
+      jobName: string;
+    },
+  ];
+}
+const Mypage = async () => {
+  try {
+    const token = localStorage.getItem('accessToken');
+
+    if (!token) {
+      throw new Error('인증 토큰이 없습니다');
+    }
+
+    const response = await api.get('/v1/member/info', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data);
+    return response.data.data;
+  } catch (error) {
+    console.error('내 계정 정보를 불러오는 것에 실패했습니다.', error);
+    throw error;
+  }
+};
+
+export const useGetInfo = () => {
+  return useQuery<GetInfoProps>({
+    queryKey: ['Mypage'],
+    queryFn: () => Mypage(),
+  });
+};
--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,54 +1,20 @@
-import BaseImage from '@assets/images/profile.png';
-import Camera from '@assets/icons/camera.svg?react';
-import Trash from '@assets/icons/profile_delete.svg?react';
-import Edit from '@assets/icons/edit-nickname.svg?react';
 import { useState } from 'react';
 import PasswordChangeModal from '@common/modal/PasswordChangeModal';
-import AddressModal from '@pages/signup/components/AddressModal';
 import LikeJobModal from '@common/modal/LikeJobModal';
 import Quit from '@common/modal/Quit';
 import useLogout from '@hook/useLogout';
+import { useGetInfo } from '@hook/mypage/useMypageQuery';
+import Nickname from './components/Nickname';
+import UpdateRegion from './components/UpdateRegion';
+import ProfileImageUploader from './components/ProfileImageUploader';
 
 const Mypage = () => {
-  const [nickname, setNickname] = useState('큐시즘');
-  const [tempNickname, setTempNickname] = useState(nickname);
-  const [isEditing, setIsEditing] = useState(false);
   const [passwordModal, setIsPasswordModal] = useState(false);
-  const [regionModal, setIsRegionModal] = useState(false);
   const [likeJob, setIsLikeJob] = useState(false);
   const [quit, setIsQuit] = useState(false);
-  const [imageSrc, setImageSrc] = useState<string>(BaseImage);
   const logout = useLogout();
-  const isDefaultImage = imageSrc === BaseImage;
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      if (imageSrc !== BaseImage) {
-        URL.revokeObjectURL(imageSrc);
-      }
-      const imageUrl = URL.createObjectURL(file);
-      setImageSrc(imageUrl);
-    }
-  };
-
-  const handleResetImage = () => {
-    setImageSrc(BaseImage);
-  };
-
-  const handleEditClick = () => {
-    setTempNickname(nickname);
-    setIsEditing(true);
-  };
-
-  const handleCancel = () => {
-    setIsEditing(false);
-  };
-
-  const handleSave = () => {
-    setNickname(tempNickname);
-    setIsEditing(false);
-  };
+  const { data: InfoData } = useGetInfo();
 
   return (
     <div className="flex flex-col items-center justify-center">
@@ -56,78 +22,17 @@ const Mypage = () => {
         <div className="text-black font-T01-B">나의 계정</div>
 
         <div className="mt-[60px] flex items-center space-x-11">
-          <div className="relative h-[65px] w-[65px]">
-            <img
-              src={imageSrc}
-              alt="기본이미지"
-              className="h-full w-full rounded-full object-cover"
-            />
-            {isDefaultImage ? (
-              <>
-                <label
-                  htmlFor="profile-upload"
-                  className="absolute bottom-0 right-0 flex h-[22px] w-[22px] cursor-pointer items-center justify-center rounded-[7px] bg-white p-[5.5px] shadow-shadow2"
-                >
-                  <Camera className="h-[11px] w-[11px]" />
-                </label>
-                <input
-                  id="profile-upload"
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  hidden
-                />
-              </>
-            ) : (
-              <button
-                type="button"
-                onClick={handleResetImage}
-                className="absolute bottom-0 right-0 flex h-[22px] w-[22px] items-center justify-center rounded-[7px] bg-white p-[5.5px] shadow-shadow2"
-              >
-                <Trash className="h-[10px] w-[10px]" />
-              </button>
-            )}
-          </div>
+          <ProfileImageUploader imageUrl={InfoData?.profileImage} />
 
-          <div className="flex items-center space-x-3">
-            {isEditing ? (
-              <div className="flex items-center space-x-3">
-                <input
-                  className="flex h-11 w-[166px] items-center rounded-[10px] border border-purple-500 bg-gray-100 px-5 py-[11px] text-gray-900 font-B01-M"
-                  value={tempNickname}
-                  onChange={(e) => setTempNickname(e.target.value)}
-                />
-                <button
-                  className="flex items-center rounded-[10px] border border-gray-900 px-[10px] py-2 text-gray-900 font-B03-M"
-                  onClick={handleCancel}
-                >
-                  취소
-                </button>
-                <button
-                  className="ml-1 flex items-center rounded-[10px] bg-gray-900 px-[10px] py-2 text-white font-B03-M"
-                  onClick={handleSave}
-                >
-                  저장
-                </button>
-              </div>
-            ) : (
-              <>
-                <span className="text-gray-900 font-T04-B">{nickname} 님</span>
-                <div
-                  className="flex cursor-pointer items-center rounded-[6px] bg-white p-[6px] shadow-shadow2"
-                  onClick={handleEditClick}
-                >
-                  <Edit />
-                </div>
-              </>
-            )}
-          </div>
+          <Nickname nickname={InfoData?.nickname || ''} />
         </div>
 
         <div className="mt-10 flex w-full flex-col rounded-[20px] border border-gray-300 p-7">
           <div className="flex gap-10">
             <span className="text-gray-900 font-B01-B">아이디</span>
-            <span className="text-gray-500 font-B01-M">happy_12</span>
+            <span className="text-gray-500 font-B01-M">
+              {InfoData?.loginId}
+            </span>
           </div>
           <div className="mt-6 flex justify-between">
             <div className="space-x-6">
@@ -148,26 +53,13 @@ const Mypage = () => {
         </div>
 
         <div className="mt-5 flex w-full flex-col rounded-[20px] border border-gray-300 p-7">
-          <div className="flex justify-between">
-            <div className="flex gap-10">
-              <span className="text-gray-900 font-B01-B">거주지</span>
-              <span className="text-gray-500 font-B01-M">대전 서구</span>
-            </div>
-
-            <button
-              className="flex items-center rounded-[10px] bg-gray-900 px-[10px] py-2 text-white font-B03-M"
-              onClick={() => setIsRegionModal(true)}
-            >
-              변경
-            </button>
-            {regionModal && (
-              <AddressModal onClose={() => setIsRegionModal(false)} />
-            )}
-          </div>
+          <UpdateRegion regionName={InfoData?.regionName} />
 
           <div className="flex gap-6">
             <span className="text-gray-900 font-B01-B">생년월일</span>
-            <span className="text-gray-500 font-B01-M">1990 / 09 / 10</span>
+            <span className="text-gray-500 font-B01-M">
+              {InfoData?.birthDate}
+            </span>
           </div>
         </div>
 
@@ -176,12 +68,12 @@ const Mypage = () => {
             <div className="flex items-center gap-5">
               <span className="text-gray-900 font-B01-B">담은 직업</span>
               <div className="flex flex-row gap-4">
-                {[...Array(5)].map((_, i) => (
+                {InfoData?.jobs?.slice(0, 5).map((job) => (
                   <span
-                    key={i}
+                    key={job.jobId}
                     className="rounded-full border border-purple-500 bg-purple-100 px-3 py-2 text-purple-500 font-B03-M"
                   >
-                    요양보호사
+                    {job.jobName}
                   </span>
                 ))}
               </div>
