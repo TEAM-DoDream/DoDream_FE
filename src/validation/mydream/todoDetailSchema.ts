628effa0d098cde563a82ed029d661d42b77f0e0
Choi Yong Won <cywin0821@gmail.com>
2025-05-26 10:40:27 +0900

    feat : [#156] 편집하는거 보는 것을 위한 todoDetailSchema.ts 제작

---
+++ src/validation/mydream/todoDetailSchema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const TodoDetailSchema = z.object({
+  todoId: z.number(),
+  title: z.string(),
+  isPublic: z.boolean(),
+  memoText: z.string(),
+  images: z.array(
+    z.object({
+      imageId: z.number(),
+      imageUrl: z.string().url().or(z.string()),
+    })
+  ),
+});
+
+export type TodoDetailData = z.infer<typeof TodoDetailSchema>;
