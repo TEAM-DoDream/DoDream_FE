df98f74c5f2cd773ec4635bbdf806b2dc2f42cd2
Choi Yong Won <cywin0821@gmail.com>
2025-05-28 00:41:19 +0900

    feat : [#165] 필요없는 주석 제거

--- src/hook/todo/useTdJobsQuery.ts
+++ src/hook/todo/useTdJobsQuery.ts
@@ -18,17 +18,13 @@ export const useMdJobsQuery = () => {
     queryFn: async () => {
       try {
         const response = await api.get<MdJobsResponse>('/v1/my-dream/todo/jobs');
-        console.log('🎯 mdJobs 요청 URL: /v1/my-dream/todo/jobs');
-        console.log('🎯 mdJobs 응답 데이터:', response.data.data);
         
         if (response.data && response.data.data && Array.isArray(response.data.data)) {
           return response.data.data;
         } else {
-          console.error('mdJobs 응답이 배열이 아닙니다:', response.data);
           return [];
         }
-      } catch (error) {
-        console.error('mdJobs 요청 중 오류 발생:', error);
+      } catch {
         return [];
       }
     },
