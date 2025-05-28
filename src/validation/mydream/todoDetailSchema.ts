19220d3724518cd33945e221e828df17c7073cbc
Choi Yong Won <cywin0821@gmail.com>
2025-05-28 20:00:27 +0900

    fix : [#183] zod로 인해서, 제대로 안되던 메모 부분 드디어 수정

--- src/validation/mydream/todoDetailSchema.ts
+++ src/validation/mydream/todoDetailSchema.ts
@@ -4,14 +4,20 @@ export const TodoDetailSchema = z.object({
   todoId: z.number(),
   title: z.string(),
   isPublic: z.boolean(),
-  memoText: z.string(),
-  link: z.string(),
-  images: z.array(
-    z.object({
-      imageId: z.number(),
-      imageUrl: z.string().url().or(z.string()),
-    })
-  ),
+
+  memoText: z.string().nullable().optional(),
+
+  link: z.string().nullable().optional(),
+
+  images: z
+    .array(
+      z.object({
+        imageId: z.number(),
+        imageUrl: z.string(),
+      })
+    )
+    .nullable()
+    .optional(),
 });
 
 export type TodoDetailData = z.infer<typeof TodoDetailSchema>;
