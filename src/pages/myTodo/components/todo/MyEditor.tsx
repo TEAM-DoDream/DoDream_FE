90a145eb9e63d85dc9cd36993ccd2bfdff441481
Choi Yong Won <cywin0821@gmail.com>
2025-05-28 18:54:31 +0900

    fix : [#174] 링크 관련 코드 주석처리

--- src/pages/myTodo/components/todo/MyEditor.tsx
+++ src/pages/myTodo/components/todo/MyEditor.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import LinkEditor from '@utils/preview/LinkEditor.tsx';
+// import LinkEditor from '@utils/preview/LinkEditor.tsx';
 import CautionIcon from '@assets/icons/caution.svg?react';
 import AutoTextArea from '@pages/myTodo/components/todo/AutoTextArea.tsx';
 
@@ -9,11 +9,17 @@ interface MyEditorProps {
   value: string;
   onChange: (text: string) => void;
   readOnly?: boolean;
-  link?: string;
+  // link?: string;
   onLinkChange?: (url: string) => void;
 }
 
-const MyEditor = ({ value, onChange, readOnly = false, link = '', onLinkChange }: MyEditorProps) => {
+const MyEditor = ({
+  value,
+  onChange,
+  readOnly = false,
+  // link = '',
+  // onLinkChange,
+}: MyEditorProps) => {
   const [localValue, setLocalValue] = useState(value);
 
   useEffect(() => {
@@ -25,12 +31,12 @@ const MyEditor = ({ value, onChange, readOnly = false, link = '', onLinkChange }
     onChange(newValue);
   };
 
-  // LinkEditor에서 URL이 변경될 때 호출될 함수
-  const handleLinkChange = (url: string) => {
-    if (onLinkChange) {
-      onLinkChange(url);
-    }
-  };
+  // // LinkEditor에서 URL이 변경될 때 호출될 함수
+  // const handleLinkChange = (url: string) => {
+  //   if (onLinkChange) {
+  //     onLinkChange(url);
+  //   }
+  // };
 
   return (
     <div className="flex h-full w-[602px] flex-col overflow-y-auto rounded-2xl border bg-white p-6">
@@ -52,10 +58,14 @@ const MyEditor = ({ value, onChange, readOnly = false, link = '', onLinkChange }
         maxLength={MAX_LENGTH}
         readOnly={readOnly}
       />
-      <div className="mt-4">
-        <h2 className="text-lg font-bold text-gray-900">링크 미리보기</h2>
-        <LinkEditor readOnly={readOnly} initialUrl={link} onUrlChange={handleLinkChange} />
-      </div>
+      {/*<div className="mt-4">*/}
+      {/*  <h2 className="text-lg font-bold text-gray-900">링크 미리보기</h2>*/}
+      {/*  <LinkEditor*/}
+      {/*    readOnly={readOnly}*/}
+      {/*    initialUrl={link}*/}
+      {/*    onUrlChange={handleLinkChange}*/}
+      {/*  />*/}
+      {/*</div>*/}
     </div>
   );
 };
