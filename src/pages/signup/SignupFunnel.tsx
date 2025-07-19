178ecdd13aa0c8a59a7df2169e4c513fda44f144
chasyuss <cotn963@gmail.com>
2025-07-19 16:32:41 +0900

    chore: [#226] 입력한 이메일과 발송한 이메일 일치시키기

--- src/pages/signup/SignupFunnel.tsx
+++ src/pages/signup/SignupFunnel.tsx
@@ -3,18 +3,23 @@ import Signup from './components/Signup';
 import Signup2 from './components/Signup2';
 import SingupAgree from './components/SingupAgree';
 import SignupEmailVerify from './components/SignupEmailVerify';
+import { useState } from 'react';
 
 const SignupFunnel = () => {
   const { Funnel, setStep } = useFunnel('singupAgree');
+  const [email, setEmail] = useState('');
 
   return (
     <Funnel>
       <Step name="singupAgree">
-        <SingupAgree onNext={() => setStep('signupEmailVerify')} />
+        <SingupAgree
+          onNext={() => setStep('signupEmailVerify')}
+          setEmail={setEmail}
+        />
       </Step>
 
       <Step name="signupEmailVerify">
-        <SignupEmailVerify onNext={() => setStep('signup')} />
+        <SignupEmailVerify onNext={() => setStep('signup')} email={email} />
       </Step>
 
       <Step name="signup">
