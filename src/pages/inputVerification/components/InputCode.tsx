918d5a1502e7ec7231e2bb3decd87918b47f801b
Choi Yong Won <cywin0821@gmail.com>
2025-07-21 11:04:45 +0900

    feat : [#223] 인증번호 찾는 페이지 ui 제작 완료, 주석으로 추후 개발을 위해 아이디어 작성

--- src/pages/inputVerification/components/InputCode.tsx
+++ src/pages/inputVerification/components/InputCode.tsx
@@ -1,14 +1,14 @@
 const InputCode = () => {
   return (
-    <div className="relative w-[384px]">
+    <div className="relative min-h-[72px] w-[424px]">
       <input
         type="text"
         placeholder="인증번호를 입력하세요"
-        className="h-12 w-full rounded-lg border border-gray-300 pl-4 pr-[84px] text-sm text-gray-700 placeholder:text-gray-400 focus:outline-none"
+        className="w-full rounded-[16px] border border-gray-300 px-[20px] py-[24px] pl-4 text-gray-700 font-B02-M placeholder:text-gray-400 focus:outline-none"
       />
       <button
         type="button"
-        className="absolute right-2 top-1/2 -translate-y-1/2 rounded-md bg-gray-300 px-3 py-1.5 text-sm text-white"
+        className="absolute right-2 top-1/2 -translate-y-1/2 rounded-[10px] bg-gray-400 px-[10px] py-[8px] text-white font-B03-M"
       >
         다시 받기
       </button>
