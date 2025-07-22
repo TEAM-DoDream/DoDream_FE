93aa95e19b4bcb8a3db7d8434c3ddf766db18ed0
Choi Yong Won <cywin0821@gmail.com>
2025-07-22 11:05:52 +0900

    feat: [#230] 이메일 유효성 메시지 수정 및 인증번호 입력 스키마 추가

--- src/validation/idFind/idFindSchema.ts
+++ src/validation/idFind/idFindSchema.ts
@@ -4,7 +4,7 @@ export const idFindSchema = z.object({
   email: z
     .string()
     .min(1, { message: '이메일을 입력해주세요.' })
-    .email({ message: '올바른 이메일 형식이 아닙니다.' })
+    .email({ message: '유효하지 않은 형식의 이메일입니다. 다시 입력해주세요.' })
 });
 
 export type IdFindFormData = z.infer<typeof idFindSchema>; 
