61df8b89bf1dc51af84a964403bef42ffa6b6631
chasyuss <cotn963@gmail.com>
2025-07-24 00:23:15 +0900

    feat: [#235] 플로팅 모달 ui 변경 구현

--- src/common/modal/FloatingModal.tsx
+++ src/common/modal/FloatingModal.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react';
-import GoIcon from '@assets/icons/go.svg?react';
-import Number1 from '@assets/icons/number.svg?react';
-import Number2 from '@assets/icons/number2.svg?react';
-import { useFloatingAddJob } from '@hook/floating/FloatingAddJob';
+// import { useFloatingAddJob } from '@hook/floating/FloatingAddJob';
 import { useFloatingSubmitMutation } from '@hook/floating/FloatingButtonMutation';
-import { useNavigate } from 'react-router-dom';
 import { useMdTodoQuery } from '@hook/todo/useMdTodoQuery';
+import TwoArrow from '@assets/icons/stick_arrow.svg?react';
+import CalendarIcon from '@assets/icons/floating_calendar.svg?react';
+import Cursor from '@assets/icons/cursor.svg?react';
 
 interface FloatingModalProps {
   onClose: () => void;
@@ -15,10 +14,9 @@ interface FloatingModalProps {
 const FloatingModal = ({ onClose, onAddTask }: FloatingModalProps) => {
   const [taskText, setTaskText] = useState('');
   const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
-  const navigate = useNavigate();
   const isLoggedIn = !!localStorage.getItem('accessToken');
 
-  const { data: addjobs } = useFloatingAddJob();
+  // const { data: addjobs } = useFloatingAddJob();
   const { mutate } = useFloatingSubmitMutation();
   const { data: hasJob, isLoading } = useMdTodoQuery();
 
@@ -60,75 +58,70 @@ const FloatingModal = ({ onClose, onAddTask }: FloatingModalProps) => {
     );
   };
 
-  const isButtonActive =
-    isLoggedIn && !hasNoJob && !!taskText.trim() && selectedCategory !== null;
+  const isButtonActive = isLoggedIn && !!taskText.trim() !== null;
 
   return (
     <div className="fixed bottom-[166px] right-[80px] z-50 w-full max-w-[476px] rounded-[30px] bg-white p-[26px] shadow-shadow4">
       <div className="flex flex-col">
         <div className="flex w-full flex-row justify-between">
-          <div className="text-lg text-black font-T05-SB">나의 할 일</div>
-          <div
-            className="flex h-[38px] cursor-pointer flex-row items-center justify-center gap-[6px] rounded-lg border-[1.4px] border-purple-500 bg-white px-2 text-purple-500 font-B02-B"
-            onClick={() => navigate('/mytodo')}
-          >
-            나의 할일 가기
-            <GoIcon />
-          </div>
+          <div className="text-black font-T03-B">바로 할일 추가</div>
         </div>
 
-        <div className="mt-[30px] flex flex-col">
-          <div className="mb-4 flex flex-row items-center gap-2">
-            <Number1 />
-            <div className="text-black font-T05-SB">
-              할 일 추가할 직업을 선택해주세요
-            </div>
-          </div>
+        <div className="mt-[30px] flex flex-col items-start">
+          {hasNoJob ? (
+            <div className="flex w-full flex-col items-center justify-center">
+              <div className="flex flex-row items-center gap-[31px] px-10 py-[7px]">
+                <div className="relative inline-block">
+                  <div className="flex h-[43px] items-center rounded-[12px] bg-purple-150 px-[18px] py-[8.68px] text-[16px] font-medium leading-[20px] text-purple-500">
+                    직업 담으러 가기
+                  </div>
+                  <div className="absolute -right-[10px] bottom-[-30px]">
+                    <Cursor />
+                  </div>
+                </div>
+                <TwoArrow />
+                <CalendarIcon />
+              </div>
 
-          <div className="flex flex-nowrap gap-2 overflow-x-auto pb-[30px] no-scrollbar">
-            {addjobs?.map((addjob) => (
-              <div
-                key={addjob.todoGroupId}
-                onClick={() => setSelectedCategory(addjob.todoGroupId)}
-                className={`cursor-pointer whitespace-nowrap rounded-[10px] p-2 font-B03-SB ${
-                  selectedCategory === addjob.todoGroupId
-                    ? 'border-[0.8px] border-purple-500 bg-purple-100 text-purple-500 shadow-shadow2'
-                    : 'bg-gray-100 text-gray-500'
+              <div className="mt-4 text-[#000] font-T05-SB">
+                {' '}
+                하단의 버튼을 통해 먼저 직업을 담아주세요
+              </div>
+
+              <button className="mt-[52px] flex h-[60px] w-full items-center justify-center rounded-2xl bg-purple-150 py-[11px] text-purple-500 font-T05-SB">
+                {' '}
+                직업 담으러 가기
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="flex w-full flex-col gap-4">
+                <div className="text-black font-T05-SB">
+                  나의 할 일에 추가할 내용을 작성해주세요
+                </div>
+
+                <textarea
+                  placeholder="예) 이력서 양식 다운로드하고 내용 적어보기"
+                  className="h-[140px] w-full resize-none rounded-[20px] border border-gray-200 bg-gray-50 p-5 text-gray-900 font-B03-M placeholder:text-gray-500 placeholder:font-B02-M focus:border-purple-500 focus:outline-none"
+                  value={taskText}
+                  onChange={(e) => setTaskText(e.target.value)}
+                />
+              </div>
+
+              <button
+                className={`mt-5 h-[60px] w-full rounded-2xl py-[11px] font-T05-SB ${
+                  isButtonActive
+                    ? 'bg-purple-150 text-purple-500 hover:bg-purple-200'
+                    : 'cursor-not-allowed bg-purple-150 text-white'
                 }`}
+                onClick={handleSubmit}
+                disabled={!isButtonActive}
               >
-                {addjob.jobName}
-              </div>
-            ))}
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-row items-center gap-2">
-              <Number2 />
-              <div className="text-black font-T05-SB">
-                나의 할 일에 추가할 내용을 작성해주세요
-              </div>
-            </div>
-
-            <textarea
-              placeholder="예) 이력서 양식 다운로드하고 내용 적어보기"
-              className="h-[140px] w-full resize-none rounded-[20px] border border-gray-200 bg-gray-50 p-5 text-gray-900 font-B03-M placeholder:text-gray-500 placeholder:font-B02-M focus:border-purple-500 focus:outline-none"
-              value={taskText}
-              onChange={(e) => setTaskText(e.target.value)}
-            />
-          </div>
+                추가하기
+              </button>
+            </>
+          )}
         </div>
-
-        <button
-          className={`mt-5 h-[60px] w-full rounded-2xl py-[11px] font-T05-SB ${
-            isButtonActive
-              ? 'bg-purple-500 text-white hover:bg-purple-600'
-              : 'cursor-not-allowed bg-purple-200 text-white'
-          }`}
-          onClick={handleSubmit}
-          disabled={!isButtonActive}
-        >
-          추가하기
-        </button>
       </div>
     </div>
   );
