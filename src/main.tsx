aed275d75fe88826d3457188f6f2ea8c0928b1ed
Choi Yong Won <cywin0821@gmail.com>
2025-07-26 10:08:40 +0900

    chore : [#248] ga를 위해 라이브러리 설치 및 셋팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import ReactGA from 'react-ga4';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+// ReactGA.initialize('G-VZ2PE4BQ7W', { testMode: true });
+ReactGA.initialize('G-VZ2PE4BQ7W');
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
