7a45225052642e738cb657c93e1fc45b03689b53
chasyuss <cotn963@gmail.com>
2025-07-27 23:19:17 +0900

    feat: [#243] footer 링크 연결

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -3,11 +3,32 @@ const Footer = () => {
   return (
     <div className="bottom-0 flex h-[130px] flex-col items-center justify-center border-t border-gray-300 bg-white py-6">
       <div className="flex flex-row items-center gap-[9px] text-gray-500 font-B02-SB">
-        <span> 공지사항 </span>
+        <a
+          href="https://substantial-scabiosa-c3a.notion.site/23df45a40bd880d0993bd9633f8eb2fa"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer"
+        >
+          개인정보처리방침
+        </a>
         <Divider />
-        <span> 이용약관 </span>
+        <a
+          href="https://substantial-scabiosa-c3a.notion.site/23df45a40bd88095bcc2cde291110ee8"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer"
+        >
+          이용약관
+        </a>
         <Divider />
-        <span> 문의하기 </span>
+        <a
+          href="https://forms.gle/kYUHuiK3SzmUEQLf9"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer"
+        >
+          문의하기
+        </a>
       </div>
 
       <div className="mt-[13px] text-center text-gray-400 font-B01-SB">
