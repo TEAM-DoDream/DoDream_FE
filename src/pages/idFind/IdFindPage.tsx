9007d62c4bfb1aa96dcf9ccc23dfc1582cd26319
Choi Yong Won <cywin0821@gmail.com>
2025-07-29 17:24:05 +0900

    feat: [#264] 필요없는 디버깅 콘솔 삭제

--- src/pages/idFind/IdFindPage.tsx
+++ src/pages/idFind/IdFindPage.tsx
@@ -33,8 +33,6 @@ const IdFindPage = () => {
           });
         },
         onError: (error) => {
-          console.log('rererere');
-          console.log(error);
           alert(error);
         },
       }
