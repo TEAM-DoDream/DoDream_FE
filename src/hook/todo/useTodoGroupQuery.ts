f1ba49f9b58f5e7658b3c50a99294ddafcb472c8
Choi Yong Won <cywin0821@gmail.com>
2025-07-29 13:01:01 +0900

    fix: [#264] 사용하지 않는다고 생각한 쿼리들을 일단 주석처리함

--- src/hook/todo/useTodoGroupQuery.ts
+++ src/hook/todo/useTodoGroupQuery.ts
@@ -1,24 +1,24 @@
-import { useQuery } from '@tanstack/react-query';
-import api from '@hook/api';
-import {
-  TodoGroupSchema,
-  TodoGroupData,
-} from '@validation/mydream/todoGroupSchema';
-
-export const useTodoGroupQuery = (todoGroupId?: number) => {
-  return useQuery<TodoGroupData, Error>({
-    queryKey: ['todoGroup', todoGroupId],
-    queryFn: async () => {
-      if (typeof todoGroupId !== 'number') {
-        throw new Error('todoGroupId가 필요합니다.');
-      }
-
-      const { data } = await api.get(`/v1/my-dream/todo-group/${todoGroupId}`);
-
-      return TodoGroupSchema.parse(data.data);
-    },
-    staleTime: 0,
-    gcTime: 1000 * 60 * 5,
-    enabled: typeof todoGroupId === 'number',
-  });
-};
+// import { useQuery } from '@tanstack/react-query';
+// import api from '@hook/api';
+// import {
+//   TodoGroupSchema,
+//   TodoGroupData,
+// } from '@validation/mydream/todoGroupSchema';
+//
+// export const useTodoGroupQuery = (todoGroupId?: number) => {
+//   return useQuery<TodoGroupData, Error>({
+//     queryKey: ['todoGroup', todoGroupId],
+//     queryFn: async () => {
+//       if (typeof todoGroupId !== 'number') {
+//         throw new Error('todoGroupId가 필요합니다.');
+//       }
+//
+//       const { data } = await api.get(`/v1/my-dream/todo-group/${todoGroupId}`);
+//
+//       return TodoGroupSchema.parse(data.data);
+//     },
+//     staleTime: 0,
+//     gcTime: 1000 * 60 * 5,
+//     enabled: typeof todoGroupId === 'number',
+//   });
+// };
