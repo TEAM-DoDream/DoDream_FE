93b5d163097a2301b70027725be72515c3677139
Choi Yong Won <cywin0821@gmail.com>
2025-08-05 14:25:42 +0900

    feat: [#283] api.ts 코드 수정 (리프레시 토큰)

--- src/hook/api.ts
+++ src/hook/api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -14,6 +14,50 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalReq = error.config as AxiosRequestConfig & {
+      _retry?: boolean;
+    };
+
+    if (error.response?.status === 401 && !originalReq._retry) {
+      originalReq._retry = true;
+      try {
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (!refreshToken) throw new Error('No refresh token');
+
+        const { data: refreshRes } = await axios.post<{
+          accessToken: string;
+          refreshToken: string;
+        }>(
+          `${import.meta.env.VITE_BASE_URL}/v1/member/auth/refresh`,
+          { refreshToken },
+          { withCredentials: true }
+        );
+
+        localStorage.setItem('accessToken', refreshRes.accessToken);
+        localStorage.setItem('refreshToken', refreshRes.refreshToken);
+
+        api.defaults.headers.common['Authorization'] =
+          `Bearer ${refreshRes.accessToken}`;
+        if (originalReq.headers) {
+          originalReq.headers['Authorization'] =
+            `Bearer ${refreshRes.accessToken}`;
+        }
+
+        return api(originalReq);
+      } catch (_err) {
+        localStorage.clear();
+        window.location.href = '/login';
+        return Promise.reject(_err);
+      }
+    }
+
     return Promise.reject(error);
   }
 );
