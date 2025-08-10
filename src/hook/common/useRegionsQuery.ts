de3271050ccc601bc1b608be9c026ca451560c1c
Choi Yong Won <cywin0821@gmail.com>
2025-08-10 18:39:09 +0900

    refactor: [#292] 기존의 지역부분을 관리하던 useEffect, useState 방식을 useQuery를 이용하여 캐싱하도록 코드를 리팩토링

---
+++ src/hook/common/useRegionsQuery.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchRegions } from '@hook/common/regionApi';
+import { ParsedRegionData } from '@type/filter/region.ts';
+import { cityOptions, districtMap } from '@utils/data/job/filterOptions';
+
+export const useRegionsQuery = () => {
+  return useQuery<ParsedRegionData, Error>({
+    queryKey: ['regions'],
+    queryFn: fetchRegions,
+    staleTime: 1000 * 60 * 60,
+    gcTime: 1000 * 60 * 60 * 24,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    placeholderData: {
+      cityOptions,
+      districtMap,
+      regionList: [],
+    },
+  });
+};
