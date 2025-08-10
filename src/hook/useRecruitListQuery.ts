8a14fab26d2eea67f08334e8f29e729effad04a8
Choi Yong Won <cywin0821@gmail.com>
2025-08-10 17:12:40 +0900

    fix: [#292] placeholderData: keepPreviousData를 이용하여(useRecruitListQuery) 이전 데이터 기록하도록 수정

--- src/hook/useRecruitListQuery.ts
+++ src/hook/useRecruitListQuery.ts
@@ -1,4 +1,8 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import {
+  keepPreviousData,
+  useQuery,
+  UseQueryResult,
+} from '@tanstack/react-query';
 import { useFilterStore } from '@store/filterStore';
 import { useShallow } from 'zustand/react/shallow';
 import axios from 'axios';
@@ -36,7 +40,7 @@ export const useRecruitListQuery = (
 
       return RecruitListResponseSchema.parse(payload);
     },
-
+    placeholderData: keepPreviousData,
     staleTime: 1000 * 60 * 5,
     refetchOnWindowFocus: false,
     refetchOnMount: false,
